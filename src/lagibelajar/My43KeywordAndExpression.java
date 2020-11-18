add1188c5555e801a947ae0fb01fd788dc81d044
icedmuffin <65304518+icedmuffin@users.noreply.github.com>
2020-11-18 21:55:03 +0700

    belajar

---
+++ src/lagibelajar/My43KeywordAndExpression.java
@@ -0,0 +1,24 @@
+//16 november 2020 Malam
+
+package lagibelajar;
+
+public class My43KeywordAndExpression {
+
+	public static void main(String[] args) {
+		//43
+		
+		//keyword - kosa kata yang sudah ada "terdaftar"	
+		//expressi adalah variable, value dan operators
+
+		String variabel = "value";
+		int value = 12;
+		
+		//keywords - string dan int
+		//expressi - variabel = "value" dan value = 12
+		
+		
+		//44 java statement
+		String variabel = "value"; // ini semua adalah statement
+	}
+
+}
